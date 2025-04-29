e8ca99ae3b7f3fec4b91683c8e51457ce6c5cb14
Fedor <f.avdeev@list.ru>
2025-04-30 00:40:15 +0700

    refactor subscription

--- backend/src/modules/subscription/subscription.service.ts
+++ backend/src/modules/subscription/subscription.service.ts
@@ -8,14 +8,25 @@ import { subscriptions, users } from '../../database/schema';
 export class SubscriptionService {
   constructor(private readonly db: DrizzleService) {}
 
-  async subscribe(userId: number, channelId: number) {
+  async subscribe(userURL: string, channelURL: string) {
+    const user = await this.db.query.users.findFirst({
+      where: eq(users.url, userURL),
+    });
+
+    const channel = await this.db.query.users.findFirst({
+      where: eq(users.url, channelURL),
+    });
+
+    if(!user || !channel) {
+      return
+    }
     // Проверяем, существует ли уже подписка
     const existingSubscription = await this.db
       .select(subscriptions)
       .where(
         and(
-          eq(subscriptions.userId, userId),
-          eq(subscriptions.channelId, channelId)
+          eq(subscriptions.userId, user.id),
+          eq(subscriptions.channelId, channel.id)
         )
       )
       .execute();
@@ -28,20 +39,32 @@ export class SubscriptionService {
     return this.db
       .insert(subscriptions)
       .values({
-        userId,
-        channelId,
+        userId: user.id,
+        channelId: channel.id,
         createdAt: new Date()
       })
       .execute();
   }
 
-  async unsubscribe(userId: number, channelId: number) {
+  async unsubscribe(userURL: string, channelURL: string) {
+    const user = await this.db.query.users.findFirst({
+      where: eq(users.url, userURL),
+    });
+
+    const channel = await this.db.query.users.findFirst({
+      where: eq(users.url, channelURL),
+    });
+
+    if(!user || !channel) {
+      return
+    }
+
     const result = await this.db
       .delete(subscriptions)
       .where(
         and(
-          eq(subscriptions.userId, userId),
-          eq(subscriptions.channelId, channelId)
+          eq(subscriptions.userId, user.id),
+          eq(subscriptions.channelId, channel.id)
         )
       )
       .execute();
@@ -53,7 +76,15 @@ export class SubscriptionService {
     return result;
   }
 
-  async getSubscriptions(userId: number) {
+  async getSubscriptions(userUrl: string) {
+    const user = await this.db.query.users.findFirst({
+      where: eq(users.url, userUrl),
+    });
+
+    if(!user) {
+      return
+    }
+
     const subscriptionCounts = await this.db
       .select({
         channelId: subscriptions.channelId,
@@ -76,12 +107,20 @@ export class SubscriptionService {
         }
       })
       .innerJoin(users, eq(users.id, subscriptions.channelId))
-      .where(eq(subscriptions.userId, userId))
+      .where(eq(subscriptions.userId, user.id))
       .groupBy(users.id)
       .execute();
   }
 
-  async getSubscribers(channelId: number) {
+  async getSubscribers(channelURL: string) {
+    const channel = await this.db.query.users.findFirst({
+      where: eq(users.url, channelURL),
+    });
+
+    if(!channel) {
+      return
+    }
+
     return this.db
       .select(subscriptions, {
         subscriber: {
@@ -91,17 +130,29 @@ export class SubscriptionService {
         }
       })
       .innerJoin(users, eq(users.id, subscriptions.userId))
-      .where(eq(subscriptions.channelId, channelId))
+      .where(eq(subscriptions.channelId, channel.id))
       .execute();
   }
 
-  async checkSubscription(userId: number, channelId: number) {
+  async checkSubscription(userURL: string, channelURL: string) {
+    const user = await this.db.query.users.findFirst({
+      where: eq(users.url, userURL),
+    });
+
+    const channel = await this.db.query.users.findFirst({
+      where: eq(users.url, channelURL),
+    });
+
+    if(!user || !channel) {
+      return
+    }
+
     const subscription = await this.db
       .select(subscriptions)
       .where(
         and(
-          eq(subscriptions.userId, userId),
-          eq(subscriptions.channelId, channelId)
+          eq(subscriptions.userId, user.id),
+          eq(subscriptions.channelId, channel.id)
         )
       )
       .execute();
